217d121409cf99b19ecc6ea6f8a9882d03f9160c
Elang87 <ernesto@wondersauce.com>
2019-05-10 17:03:43 -0600

    Added mongo

--- src/db.rs
+++ src/db.rs
@@ -1,7 +1,38 @@
-use mongodb::db::ThreadedDatabase;
-use mongodb::{bson, doc, Bson};
+use mongodb::db::{Database, ThreadedDatabase};
 use mongodb::{Client, ThreadedClient};
+use std::env;
 
-pub fn create_mongo_connection((host, port): (&str, u16)) -> Client {
-    Client::connect(host, port).expect("Client failed to connect")
+/// create_mongo_connection creates a connection to a
+/// mongodb server. Returns a database.
+///
+/// # Arguments
+/// none
+///
+/// # Examples
+///
+/// ```
+/// use::db::create_mongo_connection;
+/// let database = create_mongo_connection();
+/// ```
+pub fn create_mongo_connection() -> Database {
+    let host = env::var("MONGO_HOST").expect("MONGO_HOST not set");
+    let port: u16 = env::var("MONGO_PORT")
+        .expect("MONGO_PORT not set")
+        .parse()
+        .expect("Invalid string conversion to u16");
+    let user: String = env::var("MONGO_USER").expect("MONGO_USER not set");
+    let password: String = env::var("MONGO_PASSWORD").expect("MONGO_PASSWORD not set");
+    let database: String = env::var("MONGO_DATABASE").expect("MONGO_DATABASE not set");
+
+    let client = match Client::connect(&host, port) {
+        Ok(client) => client,
+        Err(err) => panic!("Client unable to connect to mongo server, {:?}", err),
+    };
+
+    let db = client.db(&database);
+
+    match db.auth(&user, &password) {
+        Ok(_) => db,
+        Err(err) => panic!("Authentication failed, {:?}", err),
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,37 @@ use crate::controllers::push_event::register_push_event;
 use crate::db::create_mongo_connection;
 
 use actix_web::{middleware, web, App, HttpResponse, HttpServer};
+use dotenv::dotenv;
+use mongodb::db::ThreadedDatabase;
+use mongodb::{bson, doc, Bson};
+use std::env;
 
 mod controllers;
 mod db;
 mod model;
 
 fn main() -> std::io::Result<()> {
-    println!("Running server at port 5000");
+    dotenv().ok();
+
+    let env_port: u16 = env::var("ENV_PORT")
+        .expect("ENV_PORT, not set")
+        .parse()
+        .expect("Invalid conversion from String to u16");
+
+    let mongo_instance = create_mongo_connection();
+    let coll = mongo_instance.collection("events");
+
+    let doc = doc! {
+        "title": "Jaws",
+        "array": [ 1, 2, 3 ],
+    };
+
+    match coll.insert_one(doc.clone(), None) {
+        Ok(_) => (),
+        Err(err) => panic!("Failed to insert to database, {:?}", err),
+    };
+
+    println!("Running server on port {port}", port = env_port);
 
     HttpServer::new(|| {
         App::new().wrap(middleware::Logger::default()).service(
